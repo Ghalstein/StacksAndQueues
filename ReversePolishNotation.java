0b5f688ecd9498cd588118bb5bbc27a05d185c57
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-04 21:57:52 -0400

    made the string stack

--- ReversePolishNotation.java
+++ ReversePolishNotation.java
@@ -4,25 +4,47 @@ public class ReversePolishNotation {
 
 	private static String ops = "+*/-";
 
-	StackRA operands = new StackRA();
-	StackRA operators = new StackRA();
+	private static StackRA operands = new StackRA();
+	private static StackRA operators = new StackRA();
 
 	private static void parse(String expression) {
 		for (int i = 0; i < expression.length(); ++i) {
-			if (ops.contains(expression.charAt(i))) {
-				operators.push(expression.charAt(i))
+			if (ops.contains(expression.charAt(i) + "")) {
+				operators.push(expression.charAt(i) + "");
 			}
 			else if (Character.isDigit(expression.charAt(i))) {
-				operators.push(expression.charAt(i))
+				operators.push(expression.charAt(i) + "");
 			}
 		}
 	}
 
-	public static void calculate() {
-
+	private static int evaluate(String val1, String val2, String op) {
+		switch(op) {
+		case '-': return val1 - val2;
+		case '+': return val1 + val2;
+		case '*': return val1 * val2;
+		case '/': return val1 / val2;
+		}
 	}
 
+	public static int calculate() {
+
+		while (!operators.isEmpty()) {
+			int val1 = Integer.parseInt(operands.pop());
+			int val2 = Integer.parseInt(operands.pop());
+			String op = operators.pop();
+			operands.push(evaluate(val1, val2, op));
+		}
+		return operands.pop();
+	}
+
+	public static void setup(String expression) {
+		parse(expression);
+		calculate();
+	}	
+
 	public static void main(String[] args) {
+		setup("12+");
 
 	}
 }
--- StackRAString.java
+++ StackRAString.java
@@ -1,10 +1,10 @@
 public class StackRA {
 
-	private int[] arr = new int[2]; 
+	private String[] arr = new String[2]; 
 	private int size = 0;
 
 	// copires the old array to the new array
-	private void copy(int[] oldArr, int[] newArr) {
+	private void copy(String[] oldArr, String[] newArr) {
 
 		for (int i = 0; i < oldArr.length; ++i) {
 			newArr[i] = oldArr[i];
@@ -14,12 +14,12 @@ public class StackRA {
 	// resizes the old array if necessary (filled or or less than a quarter filled)
 	private void resize() {
 		if (size == arr.length) {
-			int[] newArr = new int[2 * arr.length];
+			String[] newArr = new String[2 * arr.length];
 			copy(arr, newArr);
 			arr = newArr;
 		}
 		else if (size <  arr.length / 4 && size > 1) {
-			int[] newArr = new int[arr.length / 2];
+			String[] newArr = new String[arr.length / 2];
 			copy(arr, newArr);
 			arr = newArr;
 		}
@@ -30,7 +30,7 @@ public class StackRA {
 		arr[size++] = element;
 	}
 
-	public int pop() {
+	public String pop() {
 		resize();
 		if (size > 0) {
 			return arr[--size];
@@ -49,23 +49,13 @@ public class StackRA {
 		return str;
 	}
 
-	public int peek() {
+	public String peek() {
 		if (size > 0) {
 			return arr[size - 1];
 		}
 		else {
 			return -1;
 		}
-	}
-
-	public int max() {
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < size; ++i) {
-			if (arr[i] > max) {
-				max = arr[i];
-			}
-		}
-		return max;
 	}
 
 	public boolean isEmpty() {
