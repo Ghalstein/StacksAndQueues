52e25b812d8b098f8fa8c5eb9340ba15ade2b08b
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-06 07:54:44 -0400

    added the enqueue method, head, next and data attribute to queue

---
+++ Queue.java
@@ -0,0 +1,21 @@
+public class Queue<T extends Comparable<T>> {
+
+	public Class<T> data;
+	public Queue<T> next = null;
+	public Queue<T> head = null;
+
+	public Queue(Class<T> data, Queue<T> next) {
+		this.data = data;
+		this.next = next;
+		this.head.next = this;
+	}
+
+	public void enqueue(Class<T> data) {
+		this.next = new Queue<T>(data, null);
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
