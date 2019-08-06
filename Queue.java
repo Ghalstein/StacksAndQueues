05298705d10a211868a787f75b04b7b254836c06
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-06 19:50:05 -0400

    added tail to the ned of the Queue

--- Queue.java
+++ Queue.java
@@ -3,15 +3,18 @@ public class Queue<T extends Comparable<T>> {
 	public Class<T> data;
 	public Queue<T> next = null;
 	public Queue<T> head = null;
+	public Queue<T> tail = null;
 
 	public Queue(Class<T> data, Queue<T> next) {
 		this.data = data;
 		this.next = next;
-		this.head.next = this;
+		this.head = this;
+		this.tail = this;
 	}
 
 	public void enqueue(Class<T> data) {
-		this.next = new Queue<T>(data, null);
+		this.tail.next = new Queue<T>(data, null);
+		tail = tail.next;
 	}
 
 	public Queue<T> dequeue() {
@@ -20,12 +23,18 @@ public class Queue<T extends Comparable<T>> {
 		return oldHead;
 	}
 
-	public toString() {
-		
+	public String toString() {
+		Queue<T> curr = this;
+		String str = "[";
+		while (curr != null) {
+			str += " " + curr.data;
+		}
+		str += " ]";
+		return str;
 	}
 
 	public static void main(String[] args) {
-
+ 
 	}
 
 }
