705c87c0f2c46294965cefa2d88be6a3eb57a22b
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-06 20:15:47 -0400

    max function is working properly for the Queue

--- Queue.java
+++ Queue.java
@@ -1,3 +1,5 @@
+// basic queue made of lsit nodes
+
 public class Queue<T extends Comparable<T>> {
 
 	public T data;
@@ -35,10 +37,24 @@ public class Queue<T extends Comparable<T>> {
 		return str;
 	}
 
+	public T max() {
+		Queue<T> curr = this.head.next;
+		Queue<T> max = this.head;
+
+		while (curr != null) {
+			if (curr.data.compareTo(max.data) > 0) {
+				max = curr;
+			}
+			curr = curr.next;
+		}
+		return max.data;
+	}
+
 	public static void main(String[] args) {
  		Queue<Integer> test = new Queue<>(1, null);
  		test.enqueue(2);
  		test.enqueue(3);
+ 		System.out.println(test.max());
  		System.out.println(test);
  		test.dequeue();
  		System.out.println(test);
