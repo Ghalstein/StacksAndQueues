9bba65c56d860d7ea81acee3426495d5f9c3f4c8
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-07 19:01:16 -0400

    Added the dequque and enqueue functions

--- CircularQueue.java
+++ CircularQueue.java
@@ -2,9 +2,19 @@ public class CircularQueue<T extends Comparable<T>> {
 
 	private T[] arr;
 	private int size = 0;
+	private int head = 0;
+	private int tail = 0;
 
 	public CircularQueue(int capacity) {
 		this.arr = (T[]) new Object[capacity];
+	}
+
+	public T dequeue() {
+		return arr[head++];
+	}
+
+	public T enqueue(T data) {
+		arr[++tail] = data;
 	}
 
 	public static void main(String[] args) {
