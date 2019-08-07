814e923b3d0c688a522d494b3a3ebbf8c6760801
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-07 18:54:05 -0400

    created the consturctor

--- CircularQueue.java
+++ CircularQueue.java
@@ -1,4 +1,11 @@
-public class CircularQueue {
+public class CircularQueue<T extends Comparable<T>> {
+
+	private T[] arr;
+	private int size = 0;
+
+	public CircularQueue(int capacity) {
+		this.arr = (T[]) new Object[capacity];
+	}
 
 	public static void main(String[] args) {
 		
