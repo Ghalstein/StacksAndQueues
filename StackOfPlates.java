60cc5af94dabd88d6f5021b8db965b084e192186
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-22 17:49:37 -0400

    unit testing push for stacks

--- StackOfPlates.java
+++ StackOfPlates.java
@@ -9,11 +9,32 @@ public class StackOfPlates {
 	// initial stack
 	private ArrayList<int[]> stacks = new ArrayList<>();
 	private int[] s1 = new int[5];
+	private int curr = 0;
+	private int currIndex = 0;
+	
 	public StackOfPlates() {
 		stacks.add(s1);
 	}
 
-	public static void main(String[] args) {
 
+	// adds a new value to the stack at the end
+	public void push(int value) {
+		if (currIndex == stacks.get(curr).length - 1) {
+			int[] s = new int[5];
+			stacks.add(s);
+			++curr;
+			currIndex = 0;
+		}
+		stacks.get(curr)[currIndex++] = value;
+	}
+
+	public static void main(String[] args) {
+		StackOfPlates stacks = new StackOfPlates();
+		stacks.push(1);
+		stacks.push(2);
+		stacks.push(3);
+		stacks.push(4);
+		stacks.push(5);
+		stacks.push(6);
 	}
 }
