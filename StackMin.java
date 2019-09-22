a890eb7c109b3b1f2a263909ed6f3f7cb20646d6
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-22 01:19:59 -0400

    made the push method working

--- StackMin.java
+++ StackMin.java
@@ -2,7 +2,25 @@
 have to 
 */
 
+import java.util.*;
+
 public class StackMin {
+
+	// min property and array property
+	private int min;
+	private ArrayList<Integer> arr = new ArrayList<>();
+
+	public StackMin(int value) {
+		this.min = value;
+		this.arr.add(value);
+	}
+
+	//push method
+	public void push(int value) {
+		this.arr.add(value);
+		if (value < this.min) this.min = value;
+	}
+
 
 	public static void main(String[] args) {
 
