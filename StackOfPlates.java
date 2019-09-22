fd288df516c9f08ec1ac9666ea1504f584da9b90
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-22 17:34:46 -0400

    made an arraylist of stacks

--- StackOfPlates.java
+++ StackOfPlates.java
@@ -2,12 +2,16 @@
 Once stack exceeds capacity creates a new stack 
 */
 
+import java.util.*;
+
 public class StackOfPlates {
 
 	// initial stack
+	private ArrayList<int[]> stacks = new ArrayList<>();
 	private int[] s1 = new int[5];
-
-	
+	public StackOfPlates() {
+		stacks.add(s1);
+	}
 
 	public static void main(String[] args) {
 
