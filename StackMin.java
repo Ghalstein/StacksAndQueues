128dcb2c2fcee1146b5fbcd475e2f8cb0b3cb214
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-22 01:26:55 -0400

    pop method working

--- StackMin.java
+++ StackMin.java
@@ -21,6 +21,26 @@ public class StackMin {
 		if (value < this.min) this.min = value;
 	}
 
+	// pop method for taking things off 
+	public int pop() {
+		if (this.arr.size() == 0) return -1;
+		int popped = this.arr.remove(this.arr.size() - 1);
+		if (popped == this.min) {
+			this.min = searchForMin();
+		}
+		return popped;
+	}
+
+	private int searchForMin() {
+		int smallest = Integer.MAX_VALUE;
+		for (int curr : this.arr) {
+			if (curr < smallest) {
+				smallest = curr;
+			}
+		}
+		return smallest;
+	}
+
 
 	public static void main(String[] args) {
 
