ab7454b9021d85502288347a5655d07384862d3b
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-23 01:16:19 -0400

    made the pop() function for sort stack

--- SortStack.java
+++ SortStack.java
@@ -1,7 +1,19 @@
+/*
+I have to find a way to add values to the stack and make sure they're stored in order
+Probably have to make the push() O(n) to make it pop() off 
+until the right spot is found to push the element to the stack
+*/
+
 public class SortStack {
 
+	private StackMin stack = new StackMin();
+	private StackMin tempStack = new StackMin();
 
+	public int pop() {
+		if (stack.isEmpty()) return -1;
+		stack.pop();
+	}
 	public static main(String[] args) {
-		
+
 	}
 }
