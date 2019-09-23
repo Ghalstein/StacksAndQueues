4981061f1fc2152b51baa999336bc9cfafeeee5d
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-23 01:47:53 -0400

    unit testing is working well

--- SortStack.java
+++ SortStack.java
@@ -12,25 +12,44 @@ public class SortStack {
 	// pops the smallest element off the stack
 	public int pop() {
 		if (stack.isEmpty()) return -1;
-		stack.pop();
+		return stack.pop();
 	}
 
 	// pushes the elements on so that it is sorted with smallest on top
 	public void push(int value) {
+		if (this.stack.isEmpty()) {
+			this.stack.push(value);
+			return;
+		}
+
 		while (!this.stack.isEmpty()) {
-			if (this.stack.peek() >= value) {
+			if (this.stack.peek() >= value && value != -1) {
 				this.stack.push(value);
 				break;
 			}
-			this.tempStack.pop(this.stack.pop());
+			this.tempStack.push(this.stack.pop());
 		}
 
 		while (!this.tempStack.isEmpty()) {
-			this.stack.pop(this.tempStack.pop());
+			this.stack.push(this.tempStack.pop());
 		}
 	}
 
-	public static main(String[] args) {
+	public String toString() {
+		return stack.toString();
+	}
+
+	public static void main(String[] args) {
+		SortStack sortedStack = new SortStack();
+
+		sortedStack.push(5);
+		sortedStack.push(1);
+		System.out.println(sortedStack);
+		sortedStack.push(4);
+		sortedStack.push(3);
+		System.out.println(sortedStack);
+		sortedStack.pop();
+		System.out.println(sortedStack);
 
 	}
 }
