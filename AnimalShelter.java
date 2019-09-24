5d3271b6e0c2ee6ef77511936b9258ab8c6770fa
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-23 23:42:41 -0400

    planned out shelter

--- AnimalShelter.java
+++ AnimalShelter.java
@@ -1,6 +1,12 @@
+/*
+first-in, first-out animal shelter
+can either adopt the "oldest" or specify whether they want a dog/cat and recieve
+the oldest of that type
+*/
+
 public class AnimalShelter {
 
 	public static void main(String[] args) {
-		
+
 	}
 }
